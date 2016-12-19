af6fe41fc50382604f6d4b4259f740ba3706622a
Krzysztof Moskwa <krzysztof.moskwa@gmail.com>
2016-12-19 21:13:01 +0100

    Test2

--- Ex001_DblNum/main.cpp
+++ Ex001_DblNum/main.cpp
@@ -1,9 +1,10 @@
 
 #include <QtCore/QCoreApplication>
+#include <qdebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    qDebug()<<"Test3";
     return a.exec();
 }
