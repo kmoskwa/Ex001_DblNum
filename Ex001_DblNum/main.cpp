6a8a33b3897448a7ee20af3702677e8147f4025a
Krzysztof Moskwa <krzysztof.moskwa@gmail.com>
2016-12-19 21:24:35 +0100

    Basic structures

--- Ex001_DblNum/main.cpp
+++ Ex001_DblNum/main.cpp
@@ -1,6 +1,34 @@
 
 #include <QtCore/QCoreApplication>
 #include <qdebug>
+#include <qstring>
+#include <qlist>
+
+
+class DblNum
+{
+public:
+DblNum(void);
+virtual ~DblNum(void);
+protected:
+private:
+  void initExamples(void);
+  QMap<qlonglong, QString> _examples;
+};
+
+DblNum::DblNum(void)
+{
+}
+
+DblNum::~DblNum(void)
+{
+}
+
+void DblNum::initExamples(void)
+{
+
+}
+
 
 int main(int argc, char *argv[])
 {
