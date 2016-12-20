c15edc369ae8b3fbe3dc5ed57637ebbe71cba278
krzysztof.moskwa <krzysztof.moskwa@avid.com>
2016-12-20 07:44:14 +0100

    Added range iteration Code cleanup

--- Ex001_DblNum/main.cpp
+++ Ex001_DblNum/main.cpp
@@ -17,24 +17,26 @@ public:
   virtual ~DblNum(void);
   void iterateExamples(void);
   void iterateAll(void);
+  void iterateRange(quint64 start, quint64 end);
 protected:
 private:
   void initExamples(void);
-  QFile outputFile;
   QString outNumber(quint64 value);
+
+  QFile _outputFile;
   QMap<quint64, QString> _examples;
 };
 
 DblNum::DblNum(void)
 {
   initExamples();
-  outputFile.setFileName("out.txt");
-  outputFile.open(QIODevice::WriteOnly | QIODevice::Text);
+  _outputFile.setFileName("out.txt");
+  _outputFile.open(QIODevice::WriteOnly | QIODevice::Text);
 }
 
 DblNum::~DblNum(void)
 {
-  outputFile.close();
+  _outputFile.close();
 }
 
 void DblNum::initExamples(void)
@@ -56,9 +58,9 @@ void DblNum::initExamples(void)
   _examples[0x4018000000000000] = "6 = (1+2^-1) * 2^1025-1023";
   _examples[0x401c000000000000] = "7 = (1+2^-1+2^-2) * 2^1025-1023";
   _examples[0x0000000000000001] = "2^-1022-52 = 2^-1074 == 5 * 10^-324 (Min subnormal positive double)";
-  _examples[0x000fffffffffffff] = "2*-1022 * 2^-1022-52 == 2.2250738585072009 * 10^-308 (Ma* subnormal double)";
+  _examples[0x000fffffffffffff] = "2*-1022 * 2^-1022-52 == 2.2250738585072009 * 10^-308 (Max subnormal double)";
   _examples[0x0010000000000000] = "2^-1022 == 2.2250738585072014 * 10^-308 (Min normal positive double)";
-  _examples[0x7fefffffffffffff] = "(1 + (1 * 2^-52)) × 2^1023  == 1.7976931348623157 × 10^308 (Ma* Double)";
+  _examples[0x7fefffffffffffff] = "(1 + (1 * 2^-52)) × 2^1023  == 1.7976931348623157 × 10^308 (Max Double)";
 }
 
 QString DblNum::outNumber(quint64 value)
@@ -82,18 +84,23 @@ QString DblNum::outNumber(quint64 value)
   return out;
 }
 
+
+
 void DblNum::iterateAll(void)
 {
-  QTextStream out(&outputFile);
   quint64 min = std::numeric_limits<quint64>::min();
   quint64 max = std::numeric_limits<quint64>::max();
-  for (quint64 i = min; i <= max; i++)
-  //for (quint64 i = min; i <= 100; i++)
+  iterateRange(min, max);
+}
+
+void DblNum::iterateRange(quint64 start, quint64 end)
+{
+  QTextStream out(&_outputFile);
+  for (quint64 i = start; i <= end; i++)
   {
     quint64 val = i;
     QString line = outNumber(val).toStdString().c_str();
     out << line << endl;
-    //qint64 a = i % 0x1000;
     if ((i % 0x10000) == 0)
     {
       qDebug() << line;
